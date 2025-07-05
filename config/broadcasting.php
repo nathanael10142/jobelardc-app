afd37eac5147275efe38b2f40263cd6d0cba2768
Nathanael Hacker <nathanaelhacker6@gmail.com>
2025-07-05 02:53:40 +0200

    Add missing config/broadcasting.php for Pusher setup

---
+++ config/broadcasting.php
@@ -0,0 +1,68 @@
+    <?php
+
+    return [
+
+        /*
+        |--------------------------------------------------------------------------
+        | Default Broadcast Connection
+        |--------------------------------------------------------------------------
+        |
+        | This option controls the default broadcasting connection that will
+        | be used when an event is broadcast. A variety of broadcasting
+        | drivers are supported to handle the broadcasting of your events.
+        |
+        | Supported: "pusher", "redis", "log", "null"
+        |
+        */
+
+        'default' => env('BROADCAST_DRIVER', 'null'),
+
+        /*
+        |--------------------------------------------------------------------------
+        | Broadcast Connections
+        |--------------------------------------------------------------------------
+        |
+        | Here you may define the connection information for each broadcast driver
+        | that is supported by your application. A default configuration has
+        | been defined for each driver as an example. You may also add
+        | additional connections for your application as required.
+        |
+        */
+
+        'connections' => [
+
+            'pusher' => [
+                'driver' => 'pusher',
+                'key' => env('PUSHER_APP_KEY'),
+                'secret' => env('PUSHER_APP_SECRET'),
+                'app_id' => env('PUSHER_APP_ID'),
+                'options' => [
+                    'cluster' => env('PUSHER_APP_CLUSTER'),
+                    'host' => 'api-'.env('PUSHER_APP_CLUSTER', 'mt1').'.pusher.com',
+                    'port' => 443,
+                    'scheme' => 'https',
+                    'encrypted' => true,
+                    'useTLS' => true,
+                ],
+                'client_options' => [
+                    // Guzzle client options for when connecting to Pusher.
+                ],
+            ],
+
+            'redis' => [
+                'driver' => 'redis',
+                'connection' => 'default',
+            ],
+
+            'log' => [
+                'driver' => 'log',
+            ],
+
+            'null' => [
+                'driver' => 'null',
+            ],
+
+        ],
+
+    ];
+    
