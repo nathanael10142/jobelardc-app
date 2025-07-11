259e84d2acacd4e66042b2018a6322d69b66508d
Nathanael Hacker <nathanaelhacker6@gmail.com>
2025-07-11 13:06:31 +0200

    nathanael batera

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,12 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+                'resources/js/calls.js', // Ajouté pour les appels
+                'resources/css/chat.css', // Ajouté pour le CSS du chat
+            ],
             refresh: true,
         }),
         tailwindcss(),
